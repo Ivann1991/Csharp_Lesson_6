6a425617df1416c94c210d4848f937d0d9d11a35
Ivan <Korneychik91@yandex.ru>
2022-10-22 00:09:08 +0300

    firts commit

---
+++ examples_40/Program.cs
@@ -0,0 +1,33 @@
+﻿// напишите программу, которая на вход принимает три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+//(каждая сторона треугольника меньше суммы двух других сторон)
+Console.Clear();
+
+int usernum1 = Getusernum("Введи длину первой стороны ", "ошибка ввода");
+int usernum2 = Getusernum("Введи длину второй стороны ", "ошибка ввода");
+int usernum3 = Getusernum("Введи длину третьей стороны ", "ошибка ввода");
+
+bool result = proverkaNatreygolnik(usernum1, usernum2, usernum3);
+if (result)
+    Console.Write("треугольнику быть");
+else
+    Console.Write("тругольнику не быть");
+
+///////////////////////////
+int Getusernum(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int num))
+            return num;
+        Console.WriteLine(errorMessage);
+
+    }
+}
+
+bool proverkaNatreygolnik(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < a + b)
+        return true;
+    return false;
+}
---
+++ examples_42/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу которая будет преобразовывать десятичное число в двоичное
+Console.Clear();
+
+int usernum = Getusernum("Введи целое десятичное число ", "Ошибка ввода");
+string newnum = preobrazovanie(usernum);
+Console.Write(usernum + " -> " + newnum);
+
+
+
+
+
+
+
+
+
+///////////////////////////
+int Getusernum(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int num))
+            return num;
+        Console.WriteLine(errorMessage);
+
+    }
+}
+
+string preobrazovanie(int us)
+{
+    string FinishNum = "";
+    while (us > 0)
+    {
+        int a = us % 2;
+        FinishNum = a + FinishNum;
+        us = us / 2;
+    }
+    return FinishNum;
+
+}
